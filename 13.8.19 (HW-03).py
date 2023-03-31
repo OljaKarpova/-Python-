2bed0388c3969c0c52bf4a4c399870aa4704bf0d
OljaKarpova <olga7karpova@mail.ru>
2023-03-31 14:41:19 +0300

    Comments added

--- 13.8.19 (HW-03).py
+++ 13.8.19 (HW-03).py
@@ -1,15 +1,15 @@
 kol_uchastnikov = int(input("Введите количество билетов: "))
-summa = 0
-for i in range(kol_uchastnikov):
-    if kol_uchastnikov == 1:
-        vozrast = int(input("Введите возраст участника: "))
+summa = 0 #Заводим счетчик стоимости билетов
+for i in range(kol_uchastnikov): #Цикл, где для каждого участника будем запрашивать возраст
+    if kol_uchastnikov == 1: #Вариант, когда участник один
+        vozrast = int(input("Введите возраст участника: ")) #Формулировка запроса отличается от остальных случаев
         if vozrast < 18:
             summa = 0
         elif 18 <= vozrast < 25:
             summa = 990
         else:
             summa = 1390
-    elif 1 <  kol_uchastnikov < 4:
+    elif 1 < kol_uchastnikov < 4: #Вариант, когда участников до 3 включительно, скидка еще не действует
         k = i + 1
         vozrast = int(input("Введите возраст %d-го участника: " % (k)))
         if vozrast < 18:
@@ -18,7 +18,7 @@ for i in range(kol_uchastnikov):
             summa += 990
         else:
             summa += 1390
-    else:
+    else: #Вариант, когда участников больше 3, действует скидка
         k = i + 1
         vozrast = int(input("Введите возраст %d-го участника: " % (k)))
         if vozrast < 18:
